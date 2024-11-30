fd82210ff85ff0ea5d4322e223b6227125862419
Jeffry Sandy <jeffry.sandy@dana.id>
2024-11-30 13:07:17 +0700

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Jeffry Sandy Purnomo on 30/11/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
