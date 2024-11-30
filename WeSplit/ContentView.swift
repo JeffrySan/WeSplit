fb0b8f585f46f58fd03e51100d238feffdff52c0
Jeffry Sandy <jeffry.sandy@dana.id>
2024-11-30 13:34:09 +0700

    handing binding and state

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,17 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
+	
+	@State private var tapCount: Int = 0
+	@State private var name: String = ""
+	
+	var body: some View {
+		NavigationStack {
+			Form {
+				Section {
+					TextField("Enter your Name", text: $name)
+					Text("Your Name is \(name)")
+					Text("Hello, world!")
+					Text("Hello, world!")
+				}
+				Section {
+					Text("Hello, world!")
+					Text("Hello, world!")
+					Text("Hello, world!")
+				}
+				Button("Tap Count \(tapCount)") {
+					tapCount += 1
+				}
+			}
+			.navigationTitle("Swift UI")
+			.navigationBarTitleDisplayMode(.inline)
+		}
+	}
 }
 
 #Preview {
-    ContentView()
+	ContentView()
 }
